52ed5c13eeafa72c5bfb8293879ee57c96aea51b
zeynab jalalian <zjalalianweb@gmail.com>
2024-12-31 20:39:31 +0330

    js code

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 let date=document.querySelector('.date');
+let time_now=new Date().getHours();
+let show_time=time_now>=6 && time_now<12 ? "Good Morning" :
+time_now>=12 && time_now<16 ? "Good Afternoon" :
+time_now>=16 && time_now<19 ? "Good Evening" : "Good Night";
+date.innerHTML=show_time;
